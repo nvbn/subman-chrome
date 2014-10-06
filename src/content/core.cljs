cb24295fe0568d7fc952fcbf37b8737bf95d955c
nvbn <nvbn.rm@gmail.com>
2014-10-06 23:07:32 +0400

    Use core.async for simplifing event handling

--- src/content/core.cljs
+++ src/content/core.cljs
@@ -1,5 +1,7 @@
 (ns subman-chrome.content.core
-  (:require-macros [swiss.arrows :refer [-<>]]))
+  (:require-macros [cljs.core.async.macros :refer [go-loop go]]
+                   [swiss.arrows :refer [-<>]])
+  (:require [cljs.core.async :refer [<! >! chan]]))
 
 (def chrome (atom nil))
 
@@ -24,22 +26,28 @@
   [el cls]
   (.. el -classList (contains cls)))
 
-(defn on-hover
-  "Bind event when episode on hovered."
-  [cls callback]
-  (doseq [el (.querySelectorAll js/document "a")]
-    (.addEventListener el "mouseenter"
-                       #(callback (with-menu? el cls) (.-innerHTML el)))
-    (.addEventListener el "mouseleave" #(callback false ""))))
+(defn hover-chan
+  "Return channel for hovering."
+  [cls]
+  (let [ch (chan)]
+    (doseq [el (.querySelectorAll js/document "a")]
+      (.addEventListener el "mouseenter"
+                         #(go (>! ch {:with-menu? (with-menu? el cls)
+                                      :title (.-innerHTML el)})))
+      (.addEventListener el "mouseleave"
+                         #(go (>! ch {:with-menu? false
+                                      :title ""}))))
+    ch))
 
 (defn init!
   "Init extension for current page."
   [link-cls]
   (send-message :request :load-subtitles
                 :titles (get-titles link-cls))
-  (on-hover link-cls #(send-message :request :update-context-menu
-                                    :data {:with-menu? %1
-                                           :title %2})))
+  (go-loop [ch (hover-chan link-cls)]
+    (send-message :request :update-context-menu
+                  :data (<! ch))
+    (recur ch)))
 
 (when js/chrome
   (reset! chrome js/chrome)
